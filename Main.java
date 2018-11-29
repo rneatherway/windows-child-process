a8187346a7bce6c33214406cf08f86839688e2cd
Robin Neatherway <robin.neatherway@gmail.com>
2018-11-29 17:34:53 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.robin.temp;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		System.out.printf("Free disk space: %dM\n", Files.getFileStore(Paths.get(args[0])).getUsableSpace() / (1024 * 1024));
+		
+	}
+}
